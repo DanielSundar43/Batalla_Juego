b49c3c0e8527ffe081860725d5fddf9fbae1cf37
Justin-Hernandez318 <justinhv3018@gmail.com>
2026-04-17 22:21:57 -0500

    Creación clase Arma

---
+++ proyectobatalla1/src/main/java/batallaproyect/Arma.java
@@ -0,0 +1,30 @@
+package batallaproyect;
+
+public class Arma {
+
+    private String nombre;
+
+    private int dañoAdicional;
+
+    public Arma(String nombre, int dañoAdicional) {
+
+        this.nombre = nombre;
+
+        this.dañoAdicional = dañoAdicional;
+
+    }
+
+    public int getDañoAdicional() {
+
+        return dañoAdicional;
+
+    }
+
+    public String getNombre() {
+
+        return nombre;
+
+    }
+
+}
+ 
