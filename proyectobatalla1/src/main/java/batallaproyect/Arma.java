0995e42a5a46c5a30042370f2d32aaa8a01838d7
Justin-Hernandez318 <justinhv3018@gmail.com>
2026-04-17 22:58:28 -0500

    Creación clase Arma con daño adicional

--- proyectobatalla1/src/main/java/batallaproyect/Arma.java
+++ proyectobatalla1/src/main/java/batallaproyect/Arma.java
@@ -1,30 +1,21 @@
 package batallaproyect;
 
 public class Arma {
-
+ 
     private String nombre;
-
     private int dañoAdicional;
-
+ 
     public Arma(String nombre, int dañoAdicional) {
-
         this.nombre = nombre;
-
         this.dañoAdicional = dañoAdicional;
-
     }
-
+ 
     public int getDañoAdicional() {
-
         return dañoAdicional;
-
     }
-
+ 
     public String getNombre() {
-
         return nombre;
-
     }
-
 }
  
