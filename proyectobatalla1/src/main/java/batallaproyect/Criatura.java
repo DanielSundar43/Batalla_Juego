21f4b8132f3fac37fc7c4377d2a33a72080268ca
Daniel sundar Bonilla <danielsundar01011234@gmail.com>
2026-04-18 05:36:10 +0200

    Se agrega composición de Arma en Criatura

--- proyectobatalla1/src/main/java/batallaproyect/Criatura.java
+++ proyectobatalla1/src/main/java/batallaproyect/Criatura.java
@@ -4,12 +4,24 @@ public abstract class Criatura {
     protected String nombre;
     protected int salud;
     protected int fuerza;
+    protected Arma arma;
+
 
     public Criatura(String nombre, int salud, int fuerza) {
         this.nombre = nombre;
         this.salud = salud;
         this.fuerza = fuerza;
     }
+
+    public void equiparArma(Arma arma) {
+    this.arma = arma;
+
+    }
+    
+    public void desequiparArma() {
+    this.arma = null;
+}
+
 
     public abstract void atacar(Criatura objetivo);
 
